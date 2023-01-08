f982aee33f2cfeb8e3f208a3510d2232f2cc8d9b
bruno santos <devbrunosantos@gmail.com>
2023-01-08 18:43:38 -0300

    feat: input background when focus

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,7 +3,7 @@ export function Input<HTMLElementTagNameMap>(props: HTMLElementTagNameMap) {
     <input
       type="text"
       {...props}
-      className="w-full p-3 bg-gray-300 border-[1px] border-gray-500 rounded-md placeholder:text-brown-200 outline-0 text-brown-500"
+      className="w-full p-3 bg-gray-300 focus:bg-gray-200 border-[1px] border-gray-500 rounded-md placeholder:text-brown-200 outline-0 text-brown-500"
     />
   );
 }
