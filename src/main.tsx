0b54a727d650b8f0d7a48af506929f9de3daebe5
bruno santos <devbrunosantos@gmail.com>
2023-01-08 18:44:41 -0300

    feat: context for cart

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { App } from "./App";
+import { CartProductProvider } from "./contexts/CartProducts";
 import { ProductsCatalogProvider } from "./contexts/ProductsCatalog";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ProductsCatalogProvider>
-      <App />
+      <CartProductProvider>
+        <App />
+      </CartProductProvider>
     </ProductsCatalogProvider>
   </React.StrictMode>
 );
