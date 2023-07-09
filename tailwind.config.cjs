e0aa4ad551982feaebf953bdd4d22a52047a72f2
bruno santos <devbrunosantos@gmail.com>
2023-07-09 01:48:20 -0300

    feat: toasts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -34,6 +34,27 @@ module.exports = {
         gradientBorder:
           "linear-gradient(90deg, rgba(219,172,44,1) 0%, rgba(128,71,248,1) 100%)",
       },
+      keyframes: {
+        hide: {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
+        },
+        slideIn: {
+          from: {
+            transform: "translateX(calc(100% + var(--viewport-padding)))",
+          },
+          to: { transform: "translateX(0)" },
+        },
+        swipeOut: {
+          from: { transform: "translateX(var(--radix-toast-swipe-end-x))" },
+          to: { transform: "translateX(calc(100% + var(--viewport-padding)))" },
+        },
+      },
+      animation: {
+        hide: "hide 100ms ease-in",
+        slideIn: "slideIn 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        swipeOut: "swipeOut 100ms ease-out",
+      },
     },
   },
   plugins: [],
